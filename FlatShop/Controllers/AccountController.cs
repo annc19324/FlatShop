86bb53bbe3a8d33bdf4318c25bc39e5a8cfd1a12
Lê Thiên An <161805046+annc19324@users.noreply.github.com>
2025-04-05 23:44:06 +0700

    pdate

---
+++ FlatShop/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using FlatShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlatShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            if (Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Username == "admin" && model.Password == "123") // Thay bằng logic DB
+                {
+                    Session["User"] = model.Username;
+                    return RedirectToAction("Index", "Admin");
+                }
+                ModelState.AddModelError("", "Invalid credentials");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ FlatShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FlatShop.MvcApplication" Language="C#" %>
